ff8485a88c5f20834710cdc61dfc2caa22e26cc7
sushilujyeniya-allvest <sushil.ujyeniya@allvest.co>
2026-04-21 14:49:26 +0530

    Merge branch 'main' of https://github.com/sushilujyeniya-allvest/practice_repo into dev

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ export default function Home() {
           alt="Next.js logo"          
           width={100}
           height={20}
+
+
+
+
+          
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
